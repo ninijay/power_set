5af73a15fba757fee8690902f19b7f37b98c7d77
nini <ninijay93@gmail.com>
2017-10-24 14:39:59 +0200

    fixed powerset bash file

--- get_power_set.sh
+++ get_power_set.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
+# Interpreting each subset as a binary number, for an n element set you count from 0 to 2^n-1
+menge=(${@})
+n=${#menge[@]}
+potenzgroesse=$((1 << $n))
+i=0
 
-is_array()
-{   #detect if arg is an array, returns 0 on sucess, 1 otherwise
-    [ -z "$1" ] && return 1
-    if [ -n "$BASH" ]; then
-        declare -p ${1} 2> /dev/null | grep 'declare \-a' >/dev/null && return 0
+echo -e "{\c"
+# Looping through each subset
+while [ $i -lt $potenzgroesse ]
+do
+    teilmenge=()
+    j=0
+    while [ $j -lt $n ]
+    do
+        if [ $((( 1 << $j) & $i)) -gt 0 ]
+        then
+            teilmenge+=("${menge[$j]}")
+        fi
+        j=$(($j + 1))
+    done
+
+    if [ $(($i + 1)) -lt $potenzgroesse ]
+    then
+        echo -e "{${teilmenge[@]}},\c"
+    else
+        echo -e "{${teilmenge[@]}}\c"
     fi
-    return 1
-}
 
-getElements()
-{
-        echo -e "[${1}], \c" 
-}
-
-menge=${@}
-echo -e "[ \c"
-for x in $menge; do
-    getElements $x
+    i=$(($i + 1))
 done
-echo -e "[] ] \c"
+echo -e "}"
